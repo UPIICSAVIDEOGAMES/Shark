59ca79cace9ec73863d0a8ca90008a218bf7a805
Brandon González <bgonzalezg1505@alumno.ipn.mx>
2016-05-15 21:23:04 -0500

    Shark

---
+++ Shark/Assets/Scripts/Octopus.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Octopus : MonoBehaviour
+{
+    public float velocidad;
+	public bool activeShark = false;
+    public Transform transformOctopus = null;
+	public GameObject sharkGO;
+
+    void Start()
+    {
+        transformOctopus = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (activeShark == true)
+		{
+			sharkGO.SetActive (true);
+			sharkGO.transform.position = new Vector2 (10, 0);
+		}
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+			transformOctopus.position = transformOctopus.position + new Vector3 (0f, 1f, 0f) * Time.deltaTime * velocidad;
+        }
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			transformOctopus.Translate (Vector2.down * velocidad * Time.deltaTime);
+		}
+	}
+}
---
+++ Shark/Assets/Scripts/Shark.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shark : MonoBehaviour 
+{
+	private Transform transformShark = null;
+	public Transform target;
+	public float velocidad;
+	 
+
+	void Awake()
+	{
+		transformShark = transform;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transformShark.position += target.transform.position * velocidad * Time.deltaTime;
+		if (transformShark.position.x < -13)
+		{
+			transformShark.gameObject.SetActive (false);
+		}
+	}
+}
